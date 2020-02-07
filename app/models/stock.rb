f1f9acf4afc499a20617885c8f5692ee6732993a
Giorgi Meladze <giorgi.meladze711@gmail.com>
2020-02-07 15:57:40 +0400

    add User model and Stock search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,8 +4,13 @@ class Stock < ApplicationRecord
     #   publishable_token: Rails.application.credentials.iex_client[:API_key],
     #   endpoint: 'https://sandbox.iexapis.com/v1')
     client = IEX::Api::Client.new(
-      publishable_token: "pk_6d226e2032f4482a9bc0ae3527536b72",
+      publishable_token: "Tpk_1642d1f8687c45018e00e1e5e371bf87",
       endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol)
+
+    begin
+      new(ticker:ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
